57def1daca67bda3cee5e26b675a8cd9426c8f20
Trupesh Chapaneri <trupesh789@gmail.com>
2022-09-24 18:49:27 +0530

    Update: Task 1 Completed

--- components/UserCard.js
+++ components/UserCard.js
@@ -5,10 +5,13 @@ import {
   Text,
   Button,
   useColorModeValue,
+  SkeletonCircle,
+  SkeletonText,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import React from "react";
 
-function UserCard({id,first_name,last_name,email,avatar}) {
+function UserCard({ id, first_name, last_name, email, avatar, isLoading }) {
   return (
     <Box
       maxW="250px"
@@ -19,26 +22,27 @@ function UserCard({id,first_name,last_name,email,avatar}) {
       p={8}
       textAlign="center"
     >
-      <Avatar
-        size="lg"
-        src={
-          avatar
-        }
-        alt="Avatar Alt"
-        mb={4}
-      />
-      <Heading fontSize="lg">
-       {first_name} {last_name}
-      </Heading>
-      <Text fontWeight={600}  fontSize="sm" color="gray.500" mb={4}>
-        {email}
-      </Text>
-
-      <Link href={`/`}>
-        <Button size="sm" colorScheme="teal" variant="outline">
-          View User
-        </Button>
-      </Link>
+      {isLoading ? (
+        <React.Fragment>
+          <SkeletonCircle size="10" />
+          <SkeletonText mt="2" noOfLines={7} spacing="3" />
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Avatar size="lg" src={avatar} alt="User Img" mb={4} />
+          <Heading fontSize="lg">
+            {first_name} {last_name}
+          </Heading>
+          <Text fontWeight={600} fontSize="sm" color="gray.500" mb={4}>
+            {email}
+          </Text>
+          <Link href={`/`}>
+            <Button size="sm" colorScheme="teal" variant="outline">
+              View User
+            </Button>
+          </Link>{" "}
+        </React.Fragment>
+      )}
     </Box>
   );
 }
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,26 +1,78 @@
-import { Heading, SimpleGrid } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Spinner,
+  Stack,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { UserCard } from "../../components/UserCard";
 import { AppLayout } from "../../layouts/AppLayout";
 import { client } from "../../utils/api-client";
 
 function Users({ userList, notFound }) {
-  console.log(userList);
+  const router = useRouter();
+  const [isLoading, setLoading] = useState(false);
 
-  if (notFound) {
-    return <h1>something went wrong</h1>;
-  }
-  
+  const handlePagination = (page) => {
+    const { pathname: currPath, query: currQuery } = router;
+    currQuery.page = page;
+
+    router.push({
+      pathname: currPath,
+      query: currQuery,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => setLoading(true));
+    router.events.on("routeChangeComplete", () => setLoading(false));
+
+    return () => {
+      router.events.off("routeChangeStart", () => setLoading(true));
+      router.events.off("routeChangeComplete", () => setLoading(false));
+    };
+  }, [router.events]);
+
+  if (notFound)
+    return (
+      <Heading as="h1" textAlign="center">
+        Lol Something went worng ...
+      </Heading>
+    );
+
   return (
     <AppLayout>
-      <Heading as="h1" textAlign="center" mb="6">
-        List of Users
-      </Heading>
+      <Flex alignItems="center" justifyContent="space-between" flexWrap="wrap">
+        <Heading as="h1" textAlign="center" mb="6">
+          List of Users
+        </Heading>
+
+        <Stack direction="row" spacing={5}>
+          {Array(userList?.total_pages)
+            .fill()
+            .map((_, i) => (
+              <Button
+                key={i}
+                size="sm"
+                colorScheme="teal"
+                variant={userList?.page === i + 1 ? "solid" : "outline"}
+                onClick={() => handlePagination(i + 1)}
+              >
+                {i + 1}
+              </Button>
+            ))}
+        </Stack>
+      </Flex>
 
       <SimpleGrid minChildWidth="240px" spacing="30px">
-        {userList.data.map(({ id, first_name, last_name, email, avatar }) => (
+        {userList?.data?.map(({ id, first_name, last_name, email, avatar }) => (
           <UserCard
             key={id}
-            {...{ id, first_name, last_name, email, avatar }}
+            {...{ id, first_name, last_name, email, avatar, isLoading }}
           />
         ))}
       </SimpleGrid>
@@ -28,17 +80,18 @@ function Users({ userList, notFound }) {
   );
 }
 
-export async function getServerSideProps() {
-  const userList = await client(`users`);
+export async function getServerSideProps({ query }) {
+  const pageNo = parseInt(query?.page);
+  const userList = await client(`users?page=${pageNo || 1}`);
 
   if (!userList) {
     return {
-      props: { notFound: true }, // it will be passed to the page component as props
+      props: { notFound: true }, // It will be passed to the page component as props
     };
   }
 
   return {
-    props: { userList }, // it will be passed to the page component as props
+    props: { userList }, // It will be passed to the page component as props
   };
 }
 
